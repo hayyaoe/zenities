a05f697983520b86dc52e1a2e42f5900fbdc4b7d
hayyaoe <hayya947@gmail.com>
2024-09-15 11:32:04 +0700

    change waybar to eww

--- scripts/wifi_settings.sh
+++ scripts/wifi_settings.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-notify-send "Getting list of available Wi-Fi networks..."
 # Get a list of available wifi connections and morph it into a nice-looking list
 wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
 
