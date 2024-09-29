960aee9df35b3628eaa395e9333104d9f5bd3832
hayyaoe <hayya947@gmail.com>
2024-09-29 21:59:32 +0700

    added installation script

---
+++ UNINSTALL.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Remove symlinked dotfiles installed by stow
+cd $HOME/.config/zenities
+stow -D .
+
+# Restore the original hypr and kitty configurations (if backups exist)
+cd $HOME/.config
+if [ -d "hypr.bak" ]; then
+    rm -rf hypr
+    mv hypr.bak hypr
+fi
+
+if [ -d "kitty.bak" ]; then
+    rm -rf kitty
+    mv kitty.bak kitty
+fi
+
+# Remove cloned zenities dotfiles
+cd $HOME/.config
+rm -rf zenities
+
+# Clean up Eww
+# if [ -d "$HOME/eww" ]; then
+#     rm -rf $HOME/eww
+#     sudo rm /usr/local/bin/eww
+# fi
+
+# Restore default shell to bash
+chsh -s /bin/bash
+
+# Optionally stop and disable NetworkManager (comment these lines if you want to keep NetworkManager)
+# sudo systemctl disable NetworkManager
+# sudo systemctl stop NetworkManager
+
+# Reboot the system
+echo "Uninstallation complete. The system will now reboot."
+sudo reboot
+
