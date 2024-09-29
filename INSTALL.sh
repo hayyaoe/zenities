579891016ecef3aeb5ad46b98c28439796d2eb46
hayyaoe <hayya947@gmail.com>
2024-09-29 21:59:32 +0700

    added installation script

---
+++ INSTALL.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Install necessary packages via pacman
+sudo pacman -S --noconfirm git github-cli stow pamixer brightnessctl playerctl ncspot rofi-wayland hyprlock hypridle hyprpaper yazi neovim neofetch bottom networkmanager bluez bluez-utils rustup zsh
+
+# Clone zenities dotfiles
+cd $HOME/.config
+mv hypr hypr.bak 2>/dev/null
+mv kitty kitty.bak 2>/dev/null
+git clone https://github.com/hayyaoe/zenities
+cd zenities
+stow .
+
+# Go back to home directory
+cd $HOME
+
+# Install yay (AUR helper)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+# Install additional packages via yay
+yay -S --noconfirm cbonsai cmatrix cava python-pywal ttf-mononoki otf-hermit-nerd gvfs dbus libdbusmenu-glib libdbusmenu-gtk3 gtk-layer-shell brave-bin zoxide eza fzf thefuck
+
+# Install Powerlevel10k for zsh
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+# Eww installation
+cd $HOME
+git clone https://github.com/elkowar/eww
+cd eww
+cargo build --release --no-default-features --features=wayland
+cd target/release
+chmod +x ./eww
+sudo cp ./eww /usr/local/bin/
+
+# Network Manager setup
+sudo systemctl disable systemd-resolved
+sudo systemctl disable systemd-networkd
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+# Change shell to zsh
+chsh -s /bin/zsh
+
+# Reboot the system
+echo "Installation complete. The system will now reboot."
+sudo reboot
+
