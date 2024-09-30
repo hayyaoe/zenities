ccc55218278c746671025d2b9d01dc2c8e07edfa
hayyaoe <hayya947@gmail.com>
2024-09-30 18:34:27 +0700

    nvim config added linter for tsx jsx ts js css and scss

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -31,7 +31,7 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), 
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,19 @@
+-- Null-LS Config for Formatting and Linting
 return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+
     null_ls.setup({
       sources = {
+        -- Prettier for formatting
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" }
+        }),
       },
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    -- Keymap to format the buffer
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) -- Binding for formatting
   end,
 }
