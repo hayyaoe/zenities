45f7705d7122a2a6bed918a454bab6f8597db672
hayyaoe <hayya947@gmail.com>
2025-12-22 01:31:04 +0700

    refactor(scripts): update scripts that uses pywal into using wallust

--- .config/eww/scripts/update-color.sh
+++ .config/eww/scripts/update-color.sh
@@ -11,7 +11,7 @@ if [ ! -f "$WALLPAPER_DIR/$SELECTED_WALLPAPER.jpg" ]; then
 fi
 
 # Apply pywal colors
-wal -i "$WALLPAPER_DIR/$SELECTED_WALLPAPER.jpg" || { echo "Error: pywal failed"; exit 1; }
+wallust run "$WALLPAPER_DIR/$SELECTED_WALLPAPER.jpg" || { echo "Error: wallust failed"; exit 1; }
 
 # Reload eww
 killall eww || echo "Warning: No eww process found"
