7c1af600b70e9d74d1705100f8c90a0bfc16ecad
hayyaoe <hayya947@gmail.com>
2025-12-22 01:32:42 +0700

    chore(colors)remove .cache directory

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d3d0bd", "#0d0c09" },
-	[SchemeSel] = { "#d3d0bd", "#605D46" },
-	[SchemeOut] = { "#d3d0bd", "#C0B06D" },
-};
