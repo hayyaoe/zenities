fd702352e0e6e100cd3dc1399b4e1e3ab9885c10
hayyaoe <hayya947@gmail.com>
2026-01-06 01:23:10 +0700

    fix(hyprpaper): migrate to new block syntax and clean up conig

--- .config/eww/scripts/change-wallpaper.sh
+++ .config/eww/scripts/change-wallpaper.sh
@@ -1,20 +1,21 @@
-# Get the wallpaper filename passed as an argument
+#!/bin/bash
+
 SELECTED_WALLPAPER=$1
 WALLPAPER_DIR="$HOME/wallpapers"
 
-# Ensure the wallpaper file exists
+MONITOR=$(hyprctl monitors | grep '^Monitor' | awk 'NR==1 {print $2}')
+
 if [ -f "$WALLPAPER_DIR/$SELECTED_WALLPAPER.jpg" ]; then
-    # Set the wallpaper using the existing script logic
+ 
     SYMLINK_CONFIG_FILE="$HOME/.config/hypr/hyprpaper.conf"
     SYMLINK_LOCK_CONFIG="$HOME/.config/hypr/hyprlock.conf"
     TARGET_FILE=$(readlink -f "$SYMLINK_CONFIG_FILE")
     TARGET_FILE2=$(readlink -f "$SYMLINK_LOCK_CONFIG")
 
-    sed -i -e "s|preload = .*|preload = \$HOME/wallpapers/$SELECTED_WALLPAPER.jpg|" \
-          -e "s|wallpaper = ,.*|wallpaper = ,\$HOME/wallpapers/$SELECTED_WALLPAPER.jpg|" "$TARGET_FILE"
+    sed -i -e "s|monitor = .*|monitor = $MONITOR|" "$TARGET_FILE"
+    sed -i -e "s|path = .*|path = \$HOME/wallpapers/$SELECTED_WALLPAPER.jpg|" "$TARGET_FILE"
     sed -i -e "s|path = .*|path = \$HOME/wallpapers/$SELECTED_WALLPAPER.jpg|" "$TARGET_FILE2"
 
-    # Call the separate script to reload pywal, eww, and hyprpaper
     ~/zenities/.config/eww/scripts/update-color.sh "$SELECTED_WALLPAPER"
 else
     echo "Wallpaper not found: $SELECTED_WALLPAPER"
