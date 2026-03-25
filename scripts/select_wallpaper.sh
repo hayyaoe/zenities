338755806282a9cd7e963ccfa3448cc91f954642
hayyaoe <hayya947@gmail.com>
2026-03-25 13:37:05 +0700

    chore: adding wallpapers can now be done by placing image in wallpapers directory

--- scripts/select_wallpaper.sh
+++ scripts/select_wallpaper.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+NORMALIZE="$HOME/scripts/normalize_wallpaper.sh"
 WINDOW_NAME="wallpaper"
 
 if eww active-windows | grep -q "$WINDOW_NAME"; then
   eww close "$WINDOW_NAME"
 else
+
+  if [ -f "$NORMALIZE" ]; then
+      bash "$NORMALIZE"
+  fi
+
   eww open "$WINDOW_NAME"
 fi
 
